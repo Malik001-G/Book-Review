ca3f8edeaf38f39cf9fd95bc0b31b12f36103810
Malik001-G <adisamalikng0@gmail.com>
2024-10-09 03:28:50 +0100

    Created review and book Facotories

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
-{
+{
+
     use HasFactory;
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
