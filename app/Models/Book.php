359f23cc3f2058e9cee587bf6f7b4f3ca06b38c9
Malik001-G <adisamalikng0@gmail.com>
2024-10-13 19:08:18 +0100

    Added flters

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -40,7 +40,7 @@ class Book extends Model
 
     public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder
     {
-        return $query->having('reviews_count', '>=', $minReviews);
+        return $query->where('reviews_count', '>=', $minReviews);
     }
     private function dateRangeFilter(Builder $query, $from = null, $to = null)
     {
@@ -52,4 +52,31 @@ class Book extends Model
             $query->whereBetween('created_at', [$from, $to]);
         }
     }
+
+    public function scopePopularLastMonth(Builder $query): Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonth(), now())
+            ->highestRated(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+    public function scopePopularLast6Months(Builder $query): Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonths(6), now())
+            ->highestRated(now()->subMonths(6), now())
+            ->minReviews(2);
+    }
+    public function scopeHighestRatedLastMonth(Builder $query): Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonth(), now())
+            ->popular(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+    public function scopeHighestRatedLast6Months(Builder $query): Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonths(6), now())
+            ->popular(now()->subMonths(6), now())
+            ->minReviews(2);
+    }
 }
+
+
